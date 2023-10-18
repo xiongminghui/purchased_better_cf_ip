92fc422884778312099eff6afcfa8ab200f47466
xiongminghui <xgm930090280@hotmail.com>
2023-10-18 17:27:36 +0800

    Update purchased_cf.py

--- purchased_cf.py
+++ purchased_cf.py
@@ -1,42 +1,42 @@
+import math
+
 import requests
 import json
 import os
 api_key = os.environ.get("API_KEY")
-
+ip_token = os.environ.get("IP_TOKEN")
 def get_ip_region(ip):
-    res = requests.get(f"http://ip-api.com/json/{ip}?lang=zh-CN")
-    data = res.json()
-    region = "美国"
-    if data["country"] != "":
-        region = data["country"]
-    return str(region)
+    r = requests.get(f"https://ipinfo.io/{ip}?token={ip_token}")
+    ipData = r.json()
+    if r.status_code != 200:
+        exit(0)
+    if "country" not in ipData:
+        exit(0)
+    return str(ipData["country"])
 
-# get_ip_region("192.169.113.194")
-# exit(0)
 def add_region_to_ips(data):
     info = data["info"]
     for key in info:
         for item in info[key]:
             ip = item["ip"]
             region = get_ip_region(ip)
-            item["region"] = str(region)
+            item["region"] = region
     return data
 
 
-headers = {'Content-Type': 'application/json'}
-dataJson = {"key": api_key, "type": "v4"}
-response = requests.post('https://api.hostmonit.com/get_optimization_ip', json=dataJson, headers=headers)
-if response.status_code != 200:
-    print("获取失败！")
-    exit(0)
+# headers = {'Content-Type': 'application/json'}
+# dataJson = {"key": api_key, "type": "v4"}
+# response = requests.post('https://api.hostmonit.com/get_optimization_ip', json=dataJson, headers=headers)
+# if response.status_code != 200:
+#     print("获取失败！")
+#     exit(0)
+response = requests.get('https://raw.githubusercontent.com/xiongminghui/cloudflare-better-ip/main/html/purchased_cf_ip.json')
 data = response.json()
 if data["code"] != 200:
     exit()
 result = add_region_to_ips(data)
 
-output = json.dumps(result, indent=4)
-# print(output)
-with open("better_ip.json", "w") as file:
-    file.write(output)
-
-
+output = json.dumps(result, ensure_ascii=False)
+print(output)
+# with open("better_ip.json", "w") as file:
+#     file.write(output)
